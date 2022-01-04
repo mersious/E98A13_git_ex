81c650db8d550c80f305be1d06ffafca17c8e738
mersious <mersadwar2000@gmail.com>
2022-01-05 00:04:48 +0330

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <limits>
+#include <typeinfo>
+using  std::cin;
+using  std::cout;
+
+struct dot{
+    int x;
+    int y;
+};
+
+struct car{
+    dot initial_dot;
+    int price;
+    int quantity;
+};
+
+int main() {
+    int num;
+    float float_num = 3.14;
+    car first_car;
+    cout << typeid(first_car).name() << '\n';
+    cout << sizeof(first_car);
+}
